7c4792e548151ca31afd515f3ee0fc2ae5e040e0
vuralevren <evren@altogic.com>
2022-12-14 13:57:25 +0300

    invite button bug fixed

--- src/components/modals/add-team-members-modal.js
+++ src/components/modals/add-team-members-modal.js
@@ -1,7 +1,7 @@
 import { PlusSmIcon } from "@heroicons/react/outline";
 import { yupResolver } from "@hookform/resolvers/yup";
 import _ from "lodash";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -23,12 +23,13 @@ export default function AddTeamMembersModal({ show, setShow }) {
     handleSubmit,
     register,
     formState: { errors },
+    watch,
     setError,
   } = useForm({
     resolver: yupResolver(schema),
   });
 
-  const { workspaceSlug } = useParams("workspaceSlug");
+  const { workspaceSlug } = useParams();
   const dispatch = useDispatch();
   const workspace = useSelector(({ workspace }) =>
     _.get(workspace.workspaceList, workspaceSlug)
@@ -37,13 +38,14 @@ export default function AddTeamMembersModal({ show, setShow }) {
   const user = useSelector(({ auth }) => auth.user);
 
   const [isLoading, setIsLoading] = useState(false);
-  const [searchText, setSearchText] = useState("");
+  const [isLoadingSug, setIsLoadingSug] = useState(false);
 
-  const handleSearch = (e) => {
-    const { value } = e.target;
-    setSearchText(_.trim(value));
+  useEffect(() => {
+    handleSearch(watch("email"));
+  }, [watch("email")]);
 
-    if (_.size(value) > 2) {
+  const handleSearch = (searchText) => {
+    if (_.size(searchText) > 2) {
       dispatch(
         authActions.searchEmailOrNameRequest({
           userId: user?._id,
@@ -62,6 +64,7 @@ export default function AddTeamMembersModal({ show, setShow }) {
         email,
         onSuccess: () => {
           setIsLoading(false);
+          setIsLoadingSug(false);
           setShow(false);
           toast.success("Invitation sent successfully");
         },
@@ -71,13 +74,14 @@ export default function AddTeamMembersModal({ show, setShow }) {
             message: _.get(errorList, "items[0].message"),
           });
           setIsLoading(false);
+          setIsLoadingSug(false);
         },
       })
     );
   };
 
   const sendInviteToRegisteredUser = (email) => {
-    setIsLoading(true);
+    setIsLoadingSug(true);
     sendInvitation(email);
   };
 
@@ -118,22 +122,20 @@ export default function AddTeamMembersModal({ show, setShow }) {
                   placeholder="johndoe@example.com"
                   autoMargin={false}
                   register={register("email")}
-                  onChange={handleSearch}
-                  value={searchText}
                   error={errors.email}
                 />
               </div>
               <Button
                 type="submit"
                 className="bg-indigo-500 border border-transparent rounded-md py-2.5 px-3 ml-3 flex items-center justify-center text-sm font-medium text-white hover:bg-indigo-600"
-                disabled={isLoading}
+                loading={isLoading}
               >
                 Send invite
               </Button>
             </div>
           </form>
         </div>
-        {_.size(searchText) > 2 && !_.isEmpty(foundUsers) && (
+        {_.size(watch("email")) > 2 && !_.isEmpty(foundUsers) && (
           <div className="mt-10">
             <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">
               Suggestions
@@ -169,7 +171,7 @@ export default function AddTeamMembersModal({ show, setShow }) {
                       <Button
                         type="button"
                         className="inline-flex items-center py-2 px-3 border border-transparent rounded-full bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        disabled={isLoading}
+                        loading={isLoadingSug}
                         onClick={() => sendInviteToRegisteredUser(person.email)}
                       >
                         <PlusSmIcon
